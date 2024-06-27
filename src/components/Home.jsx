c3f52f6197cda9881e43acb017e34a291910b710
Djordje <djolej17@gmail.com>
2024-06-28 00:21:14 +0200

    Home - css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import '../App.css'
 const Home = () => {
   return (
     <div>
-      <h1 className='text-yellow-400  lg:text-5xl ml-20 2xl:text-6xl 2xl:leading-11 text-glow '>
+      <h1 className='text-yellow-400  lg:text-6xl ml-20 2xl:text-7xl 2xl:leading-11 text-glow '>
         Hello,<br></br> my name is <span>Đorđe</span>
       </h1>
     </div>
